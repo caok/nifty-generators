6c9f882f0aac5a430fd163d55178377569ff330a
Yongzhi Wang <wongyouth@gmail.com>
2011-12-20 16:28:21 +0800

    fix haml bugs

--- lib/generators/nifty/scaffold/templates/controller.rb
+++ lib/generators/nifty/scaffold/templates/controller.rb
@@ -1,3 +1,9 @@
 class <%= plural_class_name %>Controller < AuthorizedController
   inherited_resources
+
+  protected
+    def collection
+      @q = end_of_association_chain.search(params[:q])
+      @<%= instances_name %> = @q.result.page(params[:page])
+    end
 end
